f172955bffe2f7308d59fccf4fa2f333c6c4f796
mog147 <396mog@gmail.com>
2026-04-21 12:21:54 +0900

    add comment

--- js/components.js
+++ js/components.js
@@ -108,7 +108,7 @@ class SiteFooter extends HTMLElement {
           <div class="footer-content">
             <nav class="footer-sns">
               <a href="https://github.com/muuu-noir/mysite" target="_blank"><i class="fa-brands fa-github"></i></a>
-              <a href="https://www.linkedin.com/in/momoko-tezuka/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+              <a href="https://www.linkedin.com/in/momoko-tezuka" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
               <a href="https://note.com/muuu_noir/" target="_blank"><i class="fa-regular fa-note-sticky"></i></a>
             </nav>
             <div class="footer-copy">MOMOKO TEZUKA | 396 FOLIO</div>
